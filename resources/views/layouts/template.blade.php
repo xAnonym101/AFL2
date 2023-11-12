547bf7ec594ff6b4e231980631466207eb442e2c
xyasth <pkosasih@student.ciputra.ac.id>
2023-11-12 11:08:02 +0700

    template change and add about us progress

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -7,44 +7,52 @@
     <title>{{ $pagetitle }}</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
-        integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
-        integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous">
-    </script>
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 
-<body class=""style="background:rgba(45,37,26,1) ;">
-    <nav class="navbar navbar-expand-lg fixed-top" style="background-color: #96926b;">
+<body class=""style="background:{{ $bg_color }} ;">
+    <nav class="navbar navbar-expand-lg sticky-top" style="background-color: #96926b;box-shadow: 0 0 1px 1px rgb(55, 55, 55) inset;">
         <div class="container-fluid float-right">
-            <a class="text-white navbar-brand" href="#">Navbar</a>
+            {{-- <a class="text-white navbar-brand" href="#">virgo Furnisher</a> --}}
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNav" >
+
+            <div class="collapse navbar-collapse justify-content-center" id="navbarNav" >
                 <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="text-white nav-link {{ $home ?? '' }}" aria-current="page" href="/">Home</a>
+                    <li class="nav-item nav-tabs">
+                        <a class="text-black nav-link {{ $home ?? '' }}" aria-current="page" href="/">Home</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="text-white nav-link {{ $about ?? '' }}" href="/about_us">About us</a>
+                    <li class="nav-item nav-tabs">
+                        <a class="text-black nav-link {{ $about ?? '' }}" href="/about_us">About us</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="text-white nav-link {{ $contact ?? '' }}" href="/contact_us">contact us</a>
+                    <li class="nav-item nav-tabs">
+                        <a class="text-black nav-link {{ $contact ?? '' }}" href="/contact_us">contact us</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="text-white nav-link {{ $employee ?? '' }}" href="/employee">employee</a>
+                    <li class="nav-item nav-tabs">
+                        <a class="text-black nav-link {{ $employee ?? '' }}" href="/employee">employee</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="text-white nav-link {{ $product ?? '' }}" href="/product">product</a>
+                    <li class="nav-item nav-tabs">
+                        <a class="text-black nav-link {{ $product ?? '' }}" href="/product">product</a>
                     </li>
             </div>
         </div>
     </nav>
 
+    <script>
+        // JavaScript to toggle the navbar visibility using an icon button
+        document.getElementById('navbarToggleIcon').addEventListener('click', function() {
+            var navbar = document.getElementById('navbarNav');
+            if (navbar.classList.contains('show')) {
+                navbar.classList.remove('show');
+            } else {
+                navbar.classList.add('show');
+            }
+        });
+    </script>
     <div class="container">
         @yield('layout_content')
         @yield('gbr')
